f21d08894077a3da987d38b75acde30526ae3fc4
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-31 19:07:17 +0300

    размапил таблицу

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  const orders = [
+    {
+      id: 1,
+      name: "Синий"
+    },
+    {
+      id: 2,
+      name: "39-й размер"
+    },
+    {
+      id: 32,
+      name: "39,5 размер"
+    },
+    {
+      id: 65,
+      name: "40-й размер"
+    },
+    {
+      id: 4,
+      name: "41-размер"
+    },
+    {
+      id: 134,
+      name: "8GB"
+    }, {
+      id: 7,
+      name: "16GB"
+    },
+    {
+      id: 98,
+      name: "32GB"
+    },
+    {
+      id: 6,
+      name: "Space grey"
+    },
+    {
+      id: 56,
+      name: "Rose gold"
+    },
+    {
+      id: 78,
+      name: "Silver"
+    },
+    {
+      id: 135,
+      name: "Silver"
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="table_wrap">
+        <table>
+          <thead>
+            <tr >
+              <th>Статус</th>
+              <th>Товар</th>
+              <th>ID</th>
+              <th>Название</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((el) => {
+              return (
+                <tr key={el.id}>
+                  <td></td>
+                  <td>XXXX-</td>
+                  <td>{el.id}</td>
+                  <td>{el.name}</td>
+                </tr>
+              )
+            })
+            }
+
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
