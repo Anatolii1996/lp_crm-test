c6080e4943cd8ae1f21a682447d7a52ffeb92947
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-01 10:15:32 +0300

    делаю строку прозрачной

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,43 +1,104 @@
 import React, { useState, useEffect } from "react";
 
-const Row = ({ id, name, isHover, setCountSelectRow, countSelectRow}) => {
+const Row = ({ id, name, isHover, setCountSelectRow, countSelectRow }) => {
   const [isRowHower, setIsRowHower] = useState(false);
   const [isRowSelect, setIsRowSelect] = useState(false);
-useEffect(()=>{
-if(isRowSelect){
-    setCountSelectRow(countSelectRow+1);
-}else{
-    setCountSelectRow(countSelectRow-1);
-}
-}, [isRowSelect])
- 
+  const [isDeleteHover, setIsDeleteHover] = useState(false);
+
+  useEffect(() => {
+    if (isRowSelect) {
+      setCountSelectRow(countSelectRow + 1);
+    } else {
+      setCountSelectRow(countSelectRow - 1);
+    }
+  }, [isRowSelect]);
 
   return (
     <tr
-      className="row_wrap"
+      className={`row_wrap ${isDeleteHover?"transparent":""}`}
       onMouseOver={() => {
         setIsRowHower(true);
       }}
       onMouseOut={() => {
         setIsRowHower(false);
       }}
-      onClick={()=>{
-        setIsRowSelect(!isRowSelect)
-       
+      onClick={() => {
+        setIsRowSelect(!isRowSelect);
       }}
     >
       <td>
-        <div className={`${isRowHower ? "hover_border" : isRowSelect?"hover_border":""} `}></div>
+        <div
+          className={`${
+            isRowHower ? "hover_border" : isRowSelect ? "hover_border" : ""
+          } `}
+        ></div>
       </td>
-      <td className={`${!isRowHower ? isRowSelect ? "row_select" : "" : isRowSelect ? "row_select" : "hover"}`}>
+      <td
+        className={`${
+          !isRowHower
+            ? isRowSelect
+              ? "row_select"
+              : ""
+            : isRowSelect
+            ? "row_select"
+            : "hover"
+        }`}
+      >
         <img src="./Untitled.png" />
       </td>
-      <td className={`${!isRowHower ? isRowSelect ? "row_select" : "" : isRowSelect ? "row_select" : "hover"}`}>XXXX-</td>
-      <td className={`${!isRowHower ? isRowSelect ? "row_select" : "" : isRowSelect ? "row_select" : "hover"}`}>{id}</td>
-      <td className={`${!isRowHower ? isRowSelect ? "row_select" : "" : isRowSelect ? "row_select" : "hover"}`}>{name}</td>
-      {isHover && <td className="delete" title="Удалить строку">
-        ✖️
-      </td> }
+      <td
+        className={`${
+          !isRowHower
+            ? isRowSelect
+              ? "row_select"
+              : ""
+            : isRowSelect
+            ? "row_select"
+            : "hover"
+        }`}
+      >
+        XXXX-
+      </td>
+      <td
+        className={`${
+          !isRowHower
+            ? isRowSelect
+              ? "row_select"
+              : ""
+            : isRowSelect
+            ? "row_select"
+            : "hover"
+        }`}
+      >
+        {id}
+      </td>
+      <td
+        className={`${
+          !isRowHower
+            ? isRowSelect
+              ? "row_select"
+              : ""
+            : isRowSelect
+            ? "row_select"
+            : "hover"
+        }`}
+      >
+        {name}
+      </td>
+      {isHover && (
+        <td
+          className="delete"
+          title="Удалить строку"
+          onMouseOver={() => {
+            setIsDeleteHover(true);
+          }}
+          onMouseOut={() => {
+            setIsDeleteHover(false);
+          }}
+        >
+          ✖️
+        </td>
+      )}
     </tr>
   );
 };
