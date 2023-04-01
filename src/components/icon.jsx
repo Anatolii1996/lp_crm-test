47e63778a2d45600c1af11c6b896e21d70811c53
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-01 16:38:01 +0300

    добавил неактывный курсор для своей иконки

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,6 +1,6 @@
 const Icon=({el, imgIcon, setImgIcon})=>{
 return(
-    <img className={`${el==imgIcon?"transparent":""}`} 
+    <img className={`${el==imgIcon?"transparent no_select":""}`} 
     onClick={() => {
        if(el!=imgIcon){
          setImgIcon(el);
