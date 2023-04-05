4ba806957baa43289412f2496a55c981de75514c
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-05 19:49:36 +0300

    поправил

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 import { CaretDownOutlined } from "@ant-design/icons";
 import { BsFillCircleFill } from "react-icons/bs";
 
-
 const { Option } = Select;
 
-const CustomSelect = ({ arrName  }) => {
+const CustomSelect = ({ arrName }) => {
   const [value, setValue] = useState([]);
   const [open, setOpen] = useState(false);
-  
+
   const toggleOpen = () => {
     setOpen(!open);
   };
@@ -19,32 +18,33 @@ const CustomSelect = ({ arrName  }) => {
     setOpen(true);
   };
 
-  
-
   return (
     <Select
       size="small"
       mode="multiple"
       value={value}
-      
-      onClick={()=>{
-        
-        if(!open){
-          setOpen(true)
-        }
-        }}
+      // onClick={() => {
+      //   setOpen(true);
+      // }}
       onChange={handleChange}
       style={{
         width: "100%",
-        
       }}
-      suffixIcon={<CaretDownOutlined style={{ color: "black"}} onClick={toggleOpen}/>}
+      suffixIcon={
+        <CaretDownOutlined style={{ color: "black" }} onClick={toggleOpen} />
+      }
       open={open}
       dropdownStyle={{ borderRadius: 0 }}
-      menuItemSelectedIcon={<BsFillCircleFill style={{ color: "black", fontSize: "10px"}}/>}
+      menuItemSelectedIcon={
+        <BsFillCircleFill style={{ color: "black", fontSize: "10px", verticalAlign: "middle" }} />
+      }
     >
       {arrName.map((name) => (
-        <Option key={name} value={name} >
+        <Option
+          key={name}
+          value={name}
+         
+        >
           {name}
         </Option>
       ))}
